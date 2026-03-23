357572f258089c9db5fc05f2c7aabd91d1376bbb
yanaaaaaaaaaa <98395436+yanaaaaaaaaaa@users.noreply.github.com>
2026-03-23 12:57:33 +0500

    Add routing, 404 page and private route

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/app/app';
 
 const offersCount = 312;
@@ -10,6 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offersCount={offersCount} />
+    <BrowserRouter>
+      <App offersCount={offersCount} />
+    </BrowserRouter>
   </React.StrictMode>
 );
